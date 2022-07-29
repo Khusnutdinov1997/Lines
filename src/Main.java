da56a21d8ffba3a78ec9a9978658d92db94a799a
Emil <emiles1997@gmail.com>
2022-07-29 21:08:06 +0300

    дз выполнил

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+//      Задание 1
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите ваше имя: ");
+        String name = in.nextLine();
+        System.out.println("Введите выше отчество: ");
+        String patronymic = in.nextLine();
+        System.out.println("Введиет вашу фамилию: ");
+        String surname = in.nextLine();
+        in.close();
+        String fullName = surname + " " + name + " " + patronymic;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+//      Задание 2
+        String report = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнеия отчета - " + report);
+
+//      Задание 3
+        String change = fullName.replace('ё', 'е');
+        System.out.println("Данные ФИО Сотрудника - " + change);
+
+
     }
 }
